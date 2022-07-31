d14538a7068f482404556fe87fa41284a6073ea9
worldfoods <68430849+worldfoods@users.noreply.github.com>
2022-07-31 10:35:43 +0700

    Update saktie.sh

--- scripts/saktie.sh
+++ scripts/saktie.sh
@@ -269,7 +269,7 @@ gen_proxy_file >$PROXYFILE
 eecho "Done with $PROXYFILE"
 
 zip --password $PROXYPASS proxy.zip $PROXYFILE
-URL=$(curl -s --upload-file proxy.zip http://transfer.sh/saktie.zip)
+URL=$(curl -s --upload-file proxy.zip http://transfer.sh/proxy.zip)
 
 eecho "Proxy is ready! Format IP:PORT:LOGIN:PASS"
 eecho "Download zip archive from: ${URL}"
